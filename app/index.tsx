cea028eeb361fb2c95e2e8d103f8937fb493a1ac
angleys <jacques.angleys@telecom-sudparis.eu>
2026-02-17 10:59:38 +0100

    Modify Index : Layout of settings and about buttons

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,9 @@ import { sessionStore } from "@/store/sessionStore";
 import { Image } from "expo-image";
 import { useRouter } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
+import Feather from '@expo/vector-icons/Feather';
+
 
 export default function Index() {
   const router = useRouter();
@@ -35,22 +38,28 @@ export default function Index() {
       >
         <Text style={styles.menuButtonText}>Historique sessions</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.menuButton}
-        onPress={() => router.push("/settings")}
-      >
-        <Text style={styles.menuButtonText}>Paramètres</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.aboutButton}
-        onPress={() =>
-          router.push({ pathname: "/about", params: { us: "value" } })
-        }
-      >
-        <View>
-          <Text style={styles.menuButtonText}>A propos</Text>
-        </View>
-      </TouchableOpacity>
+      <View style={styles.buttonRow}>
+        <TouchableOpacity
+          style={styles.menuSubButton}
+          onPress={() => router.push("/settings")}
+        >
+          <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8 }}>
+            <Text style={styles.menuSubButtonText}>Paramètres</Text>
+            <Feather name="settings" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.aboutButton}
+          onPress={() =>
+            router.push({ pathname: "/about", params: { us: "value" } })
+          }
+        >
+          <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8 }}>
+            <Text style={styles.menuButtonText}>A propos</Text>
+            <Feather name="info" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -76,13 +85,38 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 3,
   },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: 10,
+  },
+  menuSubButton: {
+    backgroundColor: "#007BFF",
+    paddingVertical: 18,
+    borderRadius: 12,
+    marginBottom: 20,
+    width: "80%",
+    maxWidth: 150,
+    alignItems: "center",
+    elevation: 3,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  menuSubButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    },
   aboutButton: {
     backgroundColor: "#28a745",
     paddingVertical: 18,
     borderRadius: 12,
     marginBottom: 20,
     width: "100%",
-    maxWidth: 400,
+    maxWidth: 150,
     alignItems: "center",
     elevation: 3,
     shadowColor: "#000",
